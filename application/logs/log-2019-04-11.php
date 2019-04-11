9bb4dbdc05fc899d41150dd866707a10004e703e
Diego6210 <diegoalberto621@outlook.com>
2019-04-11 11:53:56 -0500

    Codigo nuevo

---
+++ application/logs/log-2019-04-11.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2019-04-11 17:26:36 --> UTF-8 Support Enabled
+DEBUG - 2019-04-11 17:26:36 --> No URI present. Default controller set.
+DEBUG - 2019-04-11 17:26:36 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-11 17:26:36 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-11 17:26:36 --> Total execution time: 0.9598
+DEBUG - 2019-04-11 17:26:39 --> UTF-8 Support Enabled
+DEBUG - 2019-04-11 17:26:39 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-11 17:26:39 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-11 17:26:40 --> Total execution time: 0.2275
+DEBUG - 2019-04-11 17:26:47 --> UTF-8 Support Enabled
+DEBUG - 2019-04-11 17:26:47 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-11 17:26:47 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-11 17:26:49 --> Total execution time: 1.4980
+DEBUG - 2019-04-11 17:26:49 --> UTF-8 Support Enabled
+DEBUG - 2019-04-11 17:26:49 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-11 17:26:49 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-11 17:26:50 --> Total execution time: 0.4517
