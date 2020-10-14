51a34b19f656135da91b62d97d60af4f45a5f388
Constanzo Basurto <manuel.basurto@gmail.com>
2020-10-14 18:50:53 -0500

    Corrección Libreria M_Pdf -> VERSION ANTERIOR

--- application/libraries/M_pdf.php
+++ application/libraries/M_pdf.php
@@ -3,13 +3,13 @@
 // libreria actualizada con un error en el servidor
 // require_once __DIR__ . './vendor/autoload.php';
 
- require_once __DIR__ . './vendor-mpdf/autoload.php';
- 
+ require_once __DIR__ . '/vendor-mpdf/autoload.php';
+
 class M_pdf {
- 
+
     public $param;
     public $pdf;
- 
+
     public function __construct($param = '"en-GB-x","A4","","",10,10,10,10,6,3')
     {
         $this->param =$param;
